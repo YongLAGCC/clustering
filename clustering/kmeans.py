f76f83985e29d0c1006872dc3b728d9e7dc28390
YongLAGCC <1291001957.yz@gmail.com>
2019-09-29 23:18:58 -0400

    finished clustering

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -1,17 +1,25 @@
 from collections import defaultdict
-from math import inf
+from math import inf, sqrt
 import random
-import csv
+import pandas as pd
 
 
 def point_avg(points):
+    center_point = [0] * len(points[0])
+    for curr_point in points:
+        for j in range(len(curr_point)):
+            center_point[j] += curr_point[j]
+    for i in range(len(center_point)):
+        center_point[i] = center_point[i] / len(points)
+
+    return center_point
     """
     Accepts a list of points, each with the same number of dimensions.
     (points can have more dimensions than 2)
     
     Returns a new point which is the center of all the points.
     """
-    raise NotImplementedError()
+    # raise NotImplementedError()
 
 
 def update_centers(data_set, assignments):
@@ -21,7 +29,17 @@ def update_centers(data_set, assignments):
     Compute the center for each of the assigned groups.
     Return `k` centers in a list
     """
-    raise NotImplementedError()
+    centers = []
+    assignment_dict = defaultdict(list)
+    for i in range(len(assignments)):
+        if assignments[i] not in assignment_dict:
+            assignment_dict[assignments[i]] = [data_set[i]]
+        else:
+            assignment_dict[assignments[i]] += [data_set[i]]
+    for j in assignment_dict:
+        centers.append(point_avg(assignment_dict[j]))
+
+    return centers
 
 
 def assign_points(data_points, centers):
@@ -39,29 +57,46 @@ def assign_points(data_points, centers):
         assignments.append(shortest_index)
     return assignments
 
-
 def distance(a, b):
+    squared_distance = 0
+    for i in range(len(a)):
+        squared_distance += (a[i] - b[i]) ** 2
+    return sqrt(squared_distance)
+
     """
     Returns the Euclidean distance between a and b
     """
-    raise NotImplementedError()
+    # raise NotImplementedError()
 
 
 def generate_k(data_set, k):
+
+    return random.sample(population=data_set, k=k)
     """
     Given `data_set`, which is an array of arrays,
     return a random set of k points from the data_set
     """
-    raise NotImplementedError()
+    # raise NotImplementedError()
 
 
 def get_list_from_dataset_file(dataset_file):
-    raise NotImplementedError()
+    dataset = pd.read_csv(dataset_file, names=['col1', 'col2'])
+    data = dataset.values.tolist()
+    return data
+
 
 
 def cost_function(clustering):
-    raise NotImplementedError()
 
+    if clustering is None or len(clustering) ==0:
+        raise Exception("missding input")
+    ret = 0
+    for ( k, v) in clustering.items():
+        centroid = point_avg(v)
+        for point in v:
+            ret += distance(centroid, point) **2
+    return ret
+    # raise NotImplementedError()
 
 def k_means(dataset_file, k):
     dataset = get_list_from_dataset_file(dataset_file)
@@ -76,3 +111,6 @@ def k_means(dataset_file, k):
     for assignment, point in zip(assignments, dataset):
         clustering[assignment].append(point)
     return clustering
+
+
+
